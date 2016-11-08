ac911808580463d5369466f3f7b3c6b0c0af0c28
CharlesAmbassa <charleslandryambassa@gmail.com>
2016-11-08 17:10:59 +0100

    Add files via upload

---
+++ inf4041_sendal_ambassa/app/build/generated/source/buildConfig/debug/org/esiea/sendal_ambassa/inf4041_sendal_ambassa/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.esiea.sendal_ambassa.inf4041_sendal_ambassa;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.esiea.sendal_ambassa.inf4041_sendal_ambassa";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
